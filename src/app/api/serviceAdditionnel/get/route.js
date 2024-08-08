ef61b81d2f30720841f42f8385a55c4f5cd2e136
mrvin100 <vincentyoumssi@gmail.com>
2024-08-08 09:33:26 +0100

    comment unused log of precharged elements

--- src/app/api/serviceAdditionnel/get/route.js
+++ src/app/api/serviceAdditionnel/get/route.js
@@ -5,7 +5,7 @@ import { prisma } from "@/lib/prisma";
 export async function GET(req, res) {
   try {
     const servicesAdditionnal = await prisma.additionalService.findMany();
-    console.log('servicesAdditionnal from database:', servicesAdditionnal);
+    // console.log('servicesAdditionnal from database:', servicesAdditionnal);
     return new Response(JSON.stringify(servicesAdditionnal), { status: 200 });
   } catch (error) {
     console.error('Error fetching services:', error);
